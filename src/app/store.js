db09ca5b7a384bf06c884b1837224b7d28c9e998
tie <nutthawit1513@gmai.com>
2022-09-15 11:08:01 +0700

    add postsReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {configureStore} from "@reduxjs/toolkit"
 
-export default configureStore({
-  reducer: () => ({}),
-})
+import {postsReducer} from "../features/posts/postsSlice"
+
+export default configureStore(
+  {
+    reducer: {
+      posts: postsReducer
+    }
+  }
+)
