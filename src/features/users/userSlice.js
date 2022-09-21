fd3ad0e7fb3b05bcdd104dba1879778dda7624f7
tie <nutthawit1513@gmai.com>
2022-09-21 12:58:44 +0700

    part6: Adding User Pages

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -20,3 +20,8 @@ const userSlice = createSlice({
 })
 
 export default userSlice.reducer
+
+export const selectAllUsers = state => state.users
+
+export const selectUserById = (state, userId) =>
+  state.users.find(user => user.id === userId)
