1c052fef754408878fb38bda0f2755f1adf9ba04
tie <nutthawit1513@gmai.com>
2022-09-22 07:48:12 +0700

    part6: Adding the Notifications List

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit";
 
 import postsReducer from "../features/posts/postsSlice";
 import usersReducer from "../features/users/userSlice";
+import notificationsReducer from "../features/notifications/notificationsSlice";
 
 export default configureStore(
   {
     reducer: {
       posts: postsReducer,
-      users: usersReducer
+      users: usersReducer,
+      notifications: notificationsReducer
     }
   }
 )
