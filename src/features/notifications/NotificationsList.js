bb01746a57e4b5f3454d67b337621ce22814a0b0
tie <nutthawit1513@gmai.com>
2022-09-22 09:27:49 +0700

    part6: Showing New Notifications

--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,20 +1,30 @@
-import React from "react";
-import {useSelector} from "react-redux";
-import {selectAllNotifications} from "./notificationsSlice";
+import React, {useLayoutEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {allNotificationsRead, selectAllNotifications} from "./notificationsSlice";
 import {selectAllUsers} from "../users/userSlice";
 import {formatDistanceToNow, parseISO} from "date-fns";
+import classnames from 'classnames'
 
 export const NotificationsList = () => {
+  const dispatch = useDispatch()
   const notifications = useSelector(selectAllNotifications)
   const users = useSelector(selectAllUsers)
+
+  useLayoutEffect(() => {
+    dispatch(allNotificationsRead())
+  })
 
   const renderedNotifications = notifications.map(notification => {
     const date = parseISO(notification.date)
     const timeAgo = formatDistanceToNow(date)
     const user = users.find(user => user.id === notification.user) || { name: 'Unknown User' }
 
+    const notificationClassname = classnames('notification', {
+      new: notification.isNew
+    })
+
     return (
-      <div key={notification.id} className="notification">
+      <div key={notification.id} className={notificationClassname}>
         <div>
           <b>{user.name}</b> {notification.message}
         </div>
